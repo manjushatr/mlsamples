93accc1db8db51b94d1cf0d6b206644cc12095d8
manjushatr <95216402+manjushatr@users.noreply.github.com>
2023-01-02 22:12:06 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,68 +1,92 @@
 import pandas as pd
 import numpy as np
-import pickle
+import matplotlib.pyplot as plt
+
 import streamlit as st
-from PIL import Image
+
+import pandas as pd
+import numpy as np
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS as stopwords
+from bs4 import BeautifulSoup
+import unicodedata
+import en_core_web_sm
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+import re
+import os
+import sys
+import json
+
+def removeEmails(x):
+    return re.sub(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+)',"", x)
+
+def removeUrls(x):
+    return re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '' , x)
+
+def removeRt(x):
+    return re.sub(r'\brt\b', '', x).strip()
+
+def removeSpecialChars(x):
+    x = re.sub(r'[^\w ]+', "", x)
+    x = ' '.join(x.split())
+    return x
+
+def removeAccentedChars(x):
+    x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return x
+
+def removeStopwords(x):
+    return ' '.join([t for t in x.split() if t not in stopwords])	
+
+def removeDupsChar(x):
+    x = re.sub("(.)\\1{2,}", "\\1", x)
+    return x
+
+def removeHTMLtags(x):
+    return BeautifulSoup(x, 'html.parser').get_text().strip()
+
+def dataClean(x):
+    x = str(x).lower().replace('\\', '').replace('_', ' ')
+    x = removeHTMLtags(x)
+    x = removeStopwords(x)
+    x = removeEmails(x)
+    x = removeUrls(x)
+    x = removeRt(x)
+    x = removeDupsChar(x)
+    x = removeAccentedChars(x)
+    x = removeSpecialChars(x)
+    x = re.sub("(.)\\1{2,}", "\\1", x)
+    return x
+
+st.title("IDENTIFICATION OF TWEETS RELATED TO DISASTER AND THOSE NOT RELATED TO DISASTER")
+
+st.write("You can enter your tweet below and the model trained on labeled tweet data provided by Kaggle can predict with above 80% accuracy whether the tweet is related to disaster or not")
+
+tweet = st.text_input(label="Enter the tweet you want to identify below:",value="", max_chars=None,placeholder="Enter your tweet here")
+
+
+def tweet_classifier(text):
+    tweet= text.apply(lambda x: dataClean(x))
+
+import spacy
+import en_core_web_lg
+nlp = en_core_web_lg.load()
+
+def get_vec(x):
+    doc = nlp(x)
+    vec = doc.vector
+    return vec
+
+tweet=tweet.apply(lambda x: get_vec(x))
 
 
 import pickle
-#pickle_out = open("classifier.pkl", "wb")
-#pickle.dump(classifier, pickle_out)
-#pickle_out.close()
-
-# loading in the model to predict on the data
-pickle_in = open('classifier.pkl', 'rb')
-classifier = pickle.load(pickle_in)
+pickled_model = pickle.load(open('cnnmodel.pkl', 'rb'))
+pickled_model.predict(tweet)
 
 
-#pickle.dump('classifier.pkl', open('classifier.pkl', 'wb'))#
-#model = pickle.load(open('classifier.pkl', 'rb'))
-#print("model loaded")
+if st.button("Predict"):
+       tweet_classifier(tweet)
 
-def welcome():
-	return 'welcome all'
-
-# defining the function which will make the prediction using
-# the data which the user inputs
-def prediction(sepal_length, sepal_width, petal_length, petal_width):
-
-	prediction = classifier.predict(
-		[[sepal_length, sepal_width, petal_length, petal_width]])
-	print(prediction)
-	return prediction
-	
-
-# this is the main function in which we define our webpage
-def main():
-	# giving the webpage a title
-	st.title("Iris Flower Prediction")
-	
-	# here we define some of the front end elements of the web page like
-	# the font and background color, the padding and the text to be displayed
-	html_temp = """
-	<div style ="background-color:yellow;padding:13px">
-	<h1 style ="color:black;text-align:center;">Streamlit Iris Flower Classifier ML App </h1>
-	</div>
-	"""
-	
-	# this line allows us to display the front end aspects we have
-	# defined in the above code
-	st.markdown(html_temp, unsafe_allow_html = True)
-	
-	# the following lines create text boxes in which the user can enter
-	# the data required to make the prediction
-	sepal_length = st.text_input("Sepal Length", "Type Here")
-	sepal_width = st.text_input("Sepal Width", "Type Here")
-	petal_length = st.text_input("Petal Length", "Type Here")
-	petal_width = st.text_input("Petal Width", "Type Here")
-	result =""
-	
-	# the below line ensures that when the button called 'Predict' is clicked,
-	# the prediction function defined above is called to make the prediction
-	# and store it in the variable result
-	if st.button("Predict"):
-		result = prediction(sepal_length, sepal_width, petal_length, petal_width)
-	st.success('The output is {}'.format(result))
-	
-if __name__=='__main__':
-	main()
